2fab8b122ff237866fda622b97299c2e93ce8e1f
arya <arya.lavasani@gmail.com>
2024-12-19 00:48:38 +0100

    [feat] init project

---
+++ src/main/java/la/arya/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package la.arya.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskmanagerApplication.class, args);
+    }
+
+}
